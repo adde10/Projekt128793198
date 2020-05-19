e6a0c76aa3e8d982ba76ff8bb6568e95bf775dd3
Andreas26372 <andreasgk@live.se>
2020-05-19 11:01:28 +0200

    First commit

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+<title>Leader of Northkorea!</title>
+</head>
+<body>
+<link rel="stylesheet" type="text/css" href="css/theme1.css">
+<ul>
+    <li><a class="active" href="index.php">&nbsp; Index</a></li>
+    <li><a href="about.php">&nbsp; Tyrants of all time</a></li>
+    <li><a href="contact.php">&nbsp; Attend to the army</a></li> 		
+</ul>
+<div style="text-align:center">
+  <h2>With great power comes great responsibility - In loving memory of the past Tyrants.</h2>
+</div>
+
+<!-- The four columns -->
+<div class="row">
+  <div class="column">
+    <img src="1.jpg" alt="Nature" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="2.jpg" alt="Snow" style="width:100%">
+  </div>
+  <div class="column">
+    <img src="3.jpg" alt="Mountains" style="width:100%">
+  </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<audio autoplay loop>
+    <source src="audio/bg1.mp3" type="audio/mp3" autoplay loop>
+</audio>
+ <link rel="Icon" href="img/favicon.gif" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+<meta charset="UTF-8">
+<title>Democratic Republic of Northkorea</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<link rel="stylesheet" type="text/css" href="css/theme1.css">
+<ul>
+  <li><a class="active" href="index.php">&nbsp; Index</a></li>
+  <li><a href="about.php">&nbsp; Tyrants of all time</a></li>
+  <li><a href="contact.php">&nbsp; Attend to the army</a></li> 				
+
+</ul>
+<div>
+<video id="video" height="1920" width="1080" style="display:block; margin: 0 auto;" autoplay loop muted>
+    <source src="video/bg1.mp4" type="video/mp4">
+</video>
+</div>
+<div>
+<audio autoplay loop>
+    <source src="audio/bg1.mp3" type="audio/mp3" autoplay loop>
+</audio>
+</div>
+</body>
+<footer>
+Skapad av Andreas Blomberg  
+</footer>
